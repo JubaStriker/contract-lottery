89fd6fb82e496a83057c55af0fc35cc8e19aded2
JubaStriker <jubastriker@gmail.com>
2023-09-01 19:43:55 +0600

    Testing successfull

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -57,5 +57,47 @@ describe('Lottery contract', () => {
         assert.equal(accounts[1], players[1])
         assert.equal(accounts[2], players[2])
         assert.equal(3, players.length)
+    });
+    it('requires a minimum amount of ether to enter', async () => {
+        try {
+            await lottery.methods.enter().send({
+                from: accounts[0],
+                value: 0
+            })
+            assert(false)
+        }
+        catch (err) {
+            assert.ok(err)
+        }
+    })
+    it('only manager can call pick winner ', async () => {
+        try {
+            await lottery.methods.pickWinner().send({
+                from: accounts[1]
+            });
+            assert(false)
+        } catch (err) {
+            assert(err)
+            console.log(err)
+        }
+    })
+    it('sends money to the winner', async () => {
+        try {
+            await lottery.methods.enter().send({
+                from: accounts[0],
+                value: web3.utils.toWei('0.002', 'ether')
+            })
+
+            const initialBalance = await web3.eth.getBalance(accounts[0]);
+
+            await lottery.methods.pickWinner().send({ from: accounts[0] })
+
+            const finalBalance = await web3.eth.getBalance(accounts[0]);
+            const difference = finalBalance - initialBalance;
+            console.log(difference);
+            assert(difference > web3.utils.toWei('0.0018', 'ether'))
+        } catch (err) {
+
+        }
     })
 })
